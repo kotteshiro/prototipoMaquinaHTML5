3a12f423f4564082747199e747f051342efa72cc
kotteshiro <kotteshiro@gmail.com>
2014-10-09 18:09:06 -0300

    fintoday

---
+++ www/js/maquinaBase.js
@@ -0,0 +1,7 @@
+/* jshint -W117, -W098 */ //desactivo correcciones de  globales, y las declaraciones no usadas
+
+windows.maquina = windows.maquina || {}; //Si no existe, creo el objeto global de la escena. 
+
+maquina.mostrarBtnListo=function(){
+    //To-Do
+};
